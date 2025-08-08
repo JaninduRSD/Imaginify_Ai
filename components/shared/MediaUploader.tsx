60cb5f17d29b5ed957c0b30d111de3d495cc11c9
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-08-08 11:52:51 +0530

    Refactor MediaUploader component for cleaner code and improved toast notifications; streamline prop handling and UI structure

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,81 +1,76 @@
-import { toast, useToast } from "@/hooks/use-toast";
+import { useToast } from "@/hooks/use-toast";
 import React from "react";
-import {CldUploadWidget} from "next-cloudinary"
+import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 
 type MediaUploaderProps = {
-    onValueChange: (value: string) => void;
-    setImage: React.Dispatch<any>;
-    publicId: string;
-    image: any;
-    type: string;
-  }
-
+  onValueChange: (value: string) => void;
+  setImage: React.Dispatch<any>;
+  publicId: string;
+  image: any;
+  type: string;
+};
 
 const MediaUploader = ({
-    onValueChange,
-    setImage,
-    image,
-    publicId,
-    type
-}: MediaUploaderProps) 
- =>{
-    const {toast} = useToast()
-    const onUploadSuccessHandler = (result: any) => {
-    
-        toast({
-          title: 'Image uploaded successfully',
-          description: '1 credit was deducted from your account',
-          duration: 5000,
-          className: 'success-toast' 
-        })
-      }
-    
-    const onUploadErrorHandler = () => {
+  onValueChange,
+  setImage,
+  image,
+  publicId,
+  type,
+}: MediaUploaderProps) => {
+  const { toast } = useToast();
+
+  const onUploadSuccessHandler = (result: any) => {
     toast({
-        title: 'Something went wrong while uploading',
-        description: 'Please try again',
-        duration: 5000,
-        className: 'error-toast' 
-    })
-    }
-    
+      title: "Image uploaded successfully",
+      description: "1 credit was deducted from your account",
+      duration: 5000,
+      className: "success-toast",
+    });
+  };
 
+  const onUploadErrorHandler = () => {
+    toast({
+      title: "Something went wrong while uploading",
+      description: "Please try again",
+      duration: 5000,
+      className: "error-toast",
+    });
+  };
 
-    return(
-      <CldUploadWidget
-        uploadPreset="jsm_imaginify"
-        options={{
-            multiple:false,
-            resourceType:"image"
-        }}
-        onSuccess={onUploadSuccessHandler}
-        onError={onUploadErrorHandler}>
+  return (
+    <CldUploadWidget
+      uploadPreset="jsm_imaginify"
+      options={{
+        multiple: false,
+        resourceType: "image",
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    >
+      {({ open }) => (
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">Original</h3>
 
-            {({open})=>(
-                <div className="flex flex-col gap-4">
-                    <h3 className="h3-bold text-dark-600">
-                        Original
-                    </h3>
+          {publicId ? (
+            <>Here Is The Image</>
+          ) : (
+            <div className="media-uploader_cta" onClick={() => open()}>
+              <div className="media-uploader_cta-image">
+                <Image
+                  src="/assets/icons/add.svg"
+                  alt="Add Image"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className="p-14-medium">Click here to upload image</p>
+            </div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
+  );
+};
 
-                    {publicId?(
-                        <>
-                        Here Is The Image
-                        </>):(
-                            <div className="media-uploader_cta" onClick={() => open()}>
-                                <div className="media-uploader_cta-image">
-                                    <Image 
-                                    src="/assets/icons/add.svg"
-                                    alt="Add Image"
-                                    width={24}
-                                    height={24}
-                                    />
-                                </div>
-                                <p className="p-14-medium">Click here to upload image</p>
-                            </div>
-                </div>
-            )}
-      </CldUploadWidget>
-    )
-}
-export default MediaUploader
+export default MediaUploader;
