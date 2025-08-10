a968f1ac24ea997ea45d185da5aa882d0828de81
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-08-10 15:48:15 +0530

    Refactor Home component to fetch and display images based on search parameters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,15 @@
 import { Collection } from '@/components/shared/Collection'
 import { navLinks } from '@/constants'
+import { getAllImages } from '@/lib/actions/image.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Home = ({searchParams}: SearchParamProps) => {
+const Home = async({searchParams}: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({page, searchQuery})
   return (
     <>
     <section className='home'>
@@ -33,7 +36,11 @@ const Home = ({searchParams}: SearchParamProps) => {
     </section>
 
     <section>
-      <Collection/>
+      <Collection
+      hasSearch= {true}
+      images={images?.data}
+      totalPages={images?.totalPage}
+      page={page} />
     </section>
     </>
   )
